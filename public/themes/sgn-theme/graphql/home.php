6834d45b73b2c0c86478d57c71c398be9188ec79
Elias Johansson <elias_johansson@hotmail.se>
2019-04-18 16:28:47 +0200

    Homepage graphql schema

--- public/themes/sgn-theme/graphql/home.php
+++ public/themes/sgn-theme/graphql/home.php
@@ -3,27 +3,117 @@
 use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type;
 
+$card = new ObjectType([
+    'name' => 'Card',
+    'fields' => [
+        'image' => Type::string(),
+        'title' => Type::string(),
+        'text' => Type::string(),
+    ],
+]);
+
+$banner = new ObjectType([
+    'name' => 'Banner',
+    'fields' => [
+        'title' => Type::string(),
+        'text' => Type::string(),
+    ],
+]);
+
+$visionsSection = new ObjectType([
+    'name' => 'VisionsSection',
+    'fields' => [
+        'title' => Type::string(),
+        'list' => Type::listOf(new ObjectType([
+            'name' => 'Vision',
+            'fields' => [
+                'image' => Type::string(),
+                'text' => Type::string(),
+            ],
+        ])),
+    ],
+]);
+
+$awardsSection = new ObjectType([
+    'name' => 'AwardsSection',
+    'fields' => [
+        'title' => Type::string(),
+        'list' => Type::listOf(new ObjectType([
+            'name' => 'Visions Section',
+            'fields' => [
+                'image' => Type::string(),
+                'description' => Type::string(),
+                'link' => Type::string(),
+            ],
+        ])),
+    ],
+]);
+
+$newsInfoBox = new ObjectType([
+    'name' => 'HomeNewsInfoBox',
+    'fields' => [
+        'title' => Type::string(),
+        'text' => Type::string(),
+        'button_label' => Type::string(),
+    ],
+]);
+
+$partners = new ObjectType([
+    'name' => 'Partners',
+    'fields' => [
+        'title' => Type::string(),
+        'list' => Type::listOf(new ObjectType([
+            'name' => 'Partner',
+            'fields' => [
+                'logo' => Type::string(),
+                'name' => Type::string(),
+                'link' => Type::string(),
+            ],
+        ])),
+    ],
+]);
+
+$formField = new ObjectType([
+    'name' => 'FormField',
+    'fields' => [
+        'label' => Type::string(),
+        'instructions' => Type::string(),
+    ],
+]);
+
+$contact = new ObjectType([
+    'name' => 'ContactSection',
+    'fields' => [
+        'title' => Type::string(),
+        'text' => Type::string(),
+        'form' => new ObjectType([
+            'name' => 'Contact Form',
+            'fields' => [
+                'name' => $formField,
+                'email' => $formField,
+                'subject' => $formField,
+                'message' => $formField,
+                'tac_label' => Type::string(),
+                'button_label' => Type::string(),
+            ],
+        ]),
+    ],
+]);
+
 $home = new ObjectType([
     'name' => 'HomeFields',
     'fields' => [
         'header' => $header,
-        'card1' => new ObjectType([
-            'name' => 'Card 1',
-            'fields' => [
-                'image' => $image,
-                'title' => Type::string(),
-                'text' => Type::string(),
-            ],
-        ]),
+        'card1' => $card,
         'quote' => Type::string(),
-        'card2' => new ObjectType([
-            'name' => 'Card 2',
-            'fields' => [
-                'image' => $image,
-                'title' => Type::string(),
-                'text' => Type::string(),
-            ],
-        ]),
+        'card2' => $card,
+        'green_banner_1' => $banner,
+        'green_banner_2' => $banner,
+        'visions' => $visionsSection,
+        'awards' => $awardsSection,
+        'news_info_box' => $newsInfoBox,
+        'partners' => $partners,
+        'contact' => $contact,
     ],
 ]);
 
